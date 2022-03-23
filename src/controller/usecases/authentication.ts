6a48244fb775a29a2bef9f350dd100cef4e49ac6
kdhttps <kdhttps@gmail.com>
2022-03-23 18:58:26 +0530

    refactor(authentication.ts): rename types

--- src/controller/usecases/authentication.ts
+++ src/controller/usecases/authentication.ts
@@ -1,11 +1,11 @@
-import { AuthenticationParams, AuthenticationResult, IAuthentication } from '@/domain/usecases/i-authentication'
+import { TAuthenticationParams, TAuthenticationResult, IAuthentication } from '@/domain/usecases/i-authentication'
 
 export class Authentication implements IAuthentication {
   constructor (
     private readonly loadAccountByEmail: ILoadAccountByEmail
   ) {}
 
-  async auth (authParams: AuthenticationParams): Promise<AuthenticationResult> {
+  async auth (authParams: TAuthenticationParams): Promise<TAuthenticationResult> {
     await this.loadAccountByEmail.get(authParams.email)
     return { accessToken: 'test' }
   }
