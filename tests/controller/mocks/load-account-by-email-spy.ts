1d625efbb622cff0b1e174ad1acfd19950e7fda3
kdhttps <kdhttps@gmail.com>
2022-03-24 15:09:08 +0530

    test(load-account-by-email-spy.ts): move class to its own file

---
+++ tests/controller/mocks/load-account-by-email-spy.ts
@@ -0,0 +1,15 @@
+import { ILoadAccountByEmail, TLoadAccountByEmailResult } from '@/controller/protocols/db/i-load-account-by-email'
+
+export class LoadAccountByEmailSpy implements ILoadAccountByEmail {
+  email: string = ''
+  result: TLoadAccountByEmailResult = {
+    id: '123',
+    email: 'valid@email.com',
+    password: 'valid@123'
+  }
+
+  async get (email: string): Promise<TLoadAccountByEmailResult> {
+    this.email = email
+    return this.result
+  }
+}
