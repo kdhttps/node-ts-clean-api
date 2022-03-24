4fb9350fab82836b561939632d3dab1f8d42a1a3
kdhttps <kdhttps@gmail.com>
2022-03-24 18:29:18 +0530

    feat(authentication.ts): add hash comparer module

--- src/controller/usecases/authentication.ts
+++ src/controller/usecases/authentication.ts
@@ -1,9 +1,11 @@
 import { TAuthenticationParams, TAuthenticationResult, IAuthentication } from '@/domain/usecases'
 import { ILoadAccountByEmail } from '@/controller/protocols'
+import { IHashComparer } from '@/controller/protocols/cryptography/i-hash-comparer'
 
 export class Authentication implements IAuthentication {
   constructor (
-    private readonly loadAccountByEmail: ILoadAccountByEmail
+    private readonly loadAccountByEmail: ILoadAccountByEmail,
+    private readonly hashComparer: IHashComparer
   ) {}
 
   async auth (authParams: TAuthenticationParams): Promise<TAuthenticationResult> {
@@ -12,6 +14,7 @@ export class Authentication implements IAuthentication {
       return null as any
     }
 
+    await this.hashComparer.compare(account.password, authParams.password)
     return { accessToken: 'test' }
   }
 }
